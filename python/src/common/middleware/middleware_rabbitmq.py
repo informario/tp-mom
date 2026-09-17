c3a79bf3858236a2d06761c148b8d87d79a2a842
mario <hernanmlagarde@gmail.com>
2026-09-17 02:47:39 -0300

    exchange

--- python/src/common/middleware/middleware_rabbitmq.py
+++ python/src/common/middleware/middleware_rabbitmq.py
@@ -64,16 +64,36 @@ class MessageMiddlewareQueueRabbitMQ(MessageMiddlewareQueue):
 class MessageMiddlewareExchangeRabbitMQ(MessageMiddlewareExchange):
     
     def __init__(self, host, exchange_name, routing_keys):
-        pass
+        self.connection = pika.BlockingConnection(pika.ConnectionParameters(host=host))
+        self.channel = self.connection.channel()
+        self.channel.exchange_declare(exchange=exchange_name, exchange_type='direct')
+        self.exchange_name = exchange_name
+        self.queue_name = None
+        self.routing_keys = routing_keys
+        return
 
     def start_consuming(self, on_message_callback):
-        pass
+        def pika_callback(ch, method, properties, body):
+            def ack():
+                ch.basic_ack(delivery_tag=method.delivery_tag)
+            def nack(requeue=True):
+                ch.basic_nack(delivery_tag=method.delivery_tag, requeue=requeue)
+            on_message_callback(body, ack, nack)
+        result = self.channel.queue_declare(queue="",exclusive=True,auto_delete=True)
+        self.queue_name = result.method.queue
+        for routing_key in self.routing_keys:
+            self.channel.queue_bind(exchange=self.exchange_name,queue=self.queue_name,routing_key=routing_key)
+            self.channel.basic_consume(queue=self.queue_name, on_message_callback=pika_callback, auto_ack=False)
+        self.channel.start_consuming()
+        return
 
     def stop_consuming(self):
-        pass
+        self.channel.stop_consuming()
+        return
 
     def send(self, message):
-        pass
+        self.channel.basic_publish(exchange=self.exchange_name, routing_key=self.routing_keys[0], body=message)
 
     def close(self):
-        pass
+        self.connection.close()
+        return
